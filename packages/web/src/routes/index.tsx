c68ffadc348016b0ccc2c398a0b0b52f1fdac29f
Bill Buchanan <bbuchanan@gmail.com>
2018-07-19 17:06:54 -0400

    added AuthRoute component

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -5,6 +5,8 @@ import { LoginConnector } from "../modules/login/LoginConnector";
 import { ForgotPasswordConnector } from "../modules/forgotPassword/ForgotPasswordConnector";
 import { ChangePasswordConnector } from "../modules/changePassword/ChangePasswordConnector";
 import { TextPage } from "../modules/TextPage";
+import { AuthRoute } from "@abb/controller";
+import { CreateListingConnector } from "../modules/listing/create/CreateListingConnector";
 
 export const Routes = () => (
   <BrowserRouter>
@@ -22,6 +24,7 @@ export const Routes = () => (
         component={ForgotPasswordConnector}
       />
       <Route path="/m" component={TextPage} />
+      <AuthRoute path="/create-listing" component={CreateListingConnector} />
     </Switch>
   </BrowserRouter>
 );
